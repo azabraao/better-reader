c0cc4e89751bacbc79ecd82b897f6d3fe5a310b6
Abraão Azevedo <az.abraao@gmail.com>
2022-05-14 13:22:07 -0300

    setup basic theme

---
+++ src/renderer/pages/home.tsx
@@ -0,0 +1,7 @@
+import { memo, ReactNode } from 'react';
+
+const Home: React.FC<ReactNode> = () => {
+  return <div className="" />;
+};
+
+export default memo(Home);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./src/renderer/**/*.{tsx,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#0F0E17',
+        white: '#FFFFFE',
+        danger: '#E53170',
+        highlight: '#FF8906',
+        muted: '#A7A9BE',
+      },
+    },
   },
   plugins: [],
 };
