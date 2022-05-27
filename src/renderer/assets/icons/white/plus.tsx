898d81c9ef43d34a897c3a65f9f4e2a28ca2fc08
Abraão Azevedo <az.abraao@gmail.com>
2022-05-27 19:14:44 -0300

    style Practices component

---
+++ src/renderer/assets/icons/white/plus.tsx
@@ -0,0 +1,37 @@
+import { memo, ReactNode } from 'react';
+
+const Plus: React.FC<ReactNode> = () => {
+  return (
+    <svg
+      width={27}
+      height={27}
+      viewBox="0 0 27 27"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.4998 24.7082C19.4829 24.7082 24.3332 19.858 24.3332 13.8749C24.3332 7.89179 19.4829 3.04155 13.4998 3.04155C7.51675 3.04155 2.6665 7.89179 2.6665 13.8749C2.6665 19.858 7.51675 24.7082 13.4998 24.7082Z"
+        stroke="#FFFFFE"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M13.5 9.54155V18.2082"
+        stroke="#FFFFFE"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.1665 13.8749H17.8332"
+        stroke="#FFFFFE"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default memo(Plus);
--- src/renderer/organisms/Practices/index.tsx
+++ src/renderer/organisms/Practices/index.tsx
@@ -1,10 +1,50 @@
 import { memo, ReactNode } from 'react';
+import EndToEnd from 'renderer/assets/icons/white/end-to-end';
+import Plus from 'renderer/assets/icons/white/plus';
+import Speed from 'renderer/assets/icons/white/speed';
+import Swiping from 'renderer/assets/icons/white/swiping';
 import { Section } from 'renderer/atoms';
 
 const Practices: React.FC<ReactNode> = () => {
   return (
     <Section title="Practices">
-      <span />
+      <div className="flex gap-4">
+        <div className="flex items-center gap-4">
+          <div className="flex flex-col gap-2 rounded-lg text-white p-2 border-white border-1">
+            <span className="font-medium whitespace-nowrap">
+              Varredura & ponta-ponta
+            </span>
+            <div className="flex gap-2 items-center">
+              <Swiping />
+              <EndToEnd />
+              <div className="ml-1 flex gap-2 items-center">
+                <Speed />
+                500ppm
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="flex flex-col gap-2 rounded-lg text-white p-2 border-white border-1">
+            <span className="font-medium whitespace-nowrap">
+              Somente Varredura
+            </span>
+            <div className="flex gap-2 items-center">
+              <Swiping />
+              <div className="ml-1 flex gap-2 items-center">
+                <Speed />
+                1000ppm
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="flex flex-col gap-2 rounded-lg text-white p-2 border-white border-1 items-center">
+            <span className="font-medium whitespace-nowrap">Add practice</span>
+            <Plus />
+          </div>
+        </div>
+      </div>
     </Section>
   );
 };
