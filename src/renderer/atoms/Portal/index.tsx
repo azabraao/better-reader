cb2d1a05266fec37c963f45db6dd8fef6fc45748
Abraão Azevedo <az.abraao@gmail.com>
2022-07-14 17:20:46 -0300

    fix Backdrop (Portal) z-index

--- src/renderer/atoms/Portal/index.tsx
+++ src/renderer/atoms/Portal/index.tsx
@@ -10,7 +10,7 @@ const Portal = ({ isOpen, onClick }: PortalProps) => {
   return (
     <div
       className={clsx(
-        'absolute top-0 right-0 bottom-0 left-0 z-0 pointer-events-none',
+        'absolute top-0 right-0 bottom-0 left-0 z-10 pointer-events-none',
         isOpen && 'opacity-100 bg-black bg-opacity-75 pointer-events-auto'
       )}
       onClick={onClick}
