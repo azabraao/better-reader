4f4eb196f727c0ac7445c79f8e07bfca71c3b62b
Abraão Azevedo <az.abraao@gmail.com>
2022-08-13 09:40:37 -0300

    use react strict mode

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,9 +1,14 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import '@fontsource/space-grotesk';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 // calling IPC exposed from preload script
 window.electron.ipcRenderer.once('ipc-example', (arg) => {
